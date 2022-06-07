079a153eb6a4ebae027c3f40d324ecbc94d1e980
Ulisses Cardoso <u244902@dac.unicamp.br>
2022-06-07 10:38:10 -0300

    using controller obj in main

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include "controller/controller.hpp"
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char** argv) {
+
+    cout << argv[0] << '\n';
+
+    Controller c;
+
+    return 0;
+}
